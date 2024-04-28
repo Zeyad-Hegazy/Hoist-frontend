577593a9ff2721fb8248676152ab306ff78d1830
Zeyad Hegazy <zhegazy409@gmail.com>
2024-04-28 18:41:52 +0300

    remove popup slice code

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import auth from "./auth";
 import employees from "./employees";
-// import popup from "./popup";
 
 const rootReducer = combineReducers({ auth, employees });
 
