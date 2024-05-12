d9cd7d7d4bdae829ecbe06fd3deba9b0d03ce7ab
Zeyad Hegazy <zhegazy409@gmail.com>
2024-05-12 05:41:29 +0300

    add search input to table component

--- src/components/UI/Table.jsx
+++ src/components/UI/Table.jsx
@@ -10,89 +10,91 @@ import TableRow from "@mui/material/TableRow";
 import { useState } from "react";
 import useTableActions from "../../utils/useTableActions";
 import TableActions from "./TableActions";
-import { Pagination } from "@mui/material";
+import { Pagination, TextField } from "@mui/material";
 
 const TableComponent = ({ columns, rows, openForm, formName }) => {
-      const { getViewHandler, getEditHandler, getDeleteHandler } = useTableActions(
-            formName,
-            openForm
-      );
+	const { getViewHandler, getEditHandler, getDeleteHandler } = useTableActions(
+		formName,
+		openForm
+	);
 
-      const [page, setPage] = useState(0);
-      const [rowsPerPage, setRowsPerPage] = useState(10);
+	const [page, setPage] = useState(0);
+	const [rowsPerPage, setRowsPerPage] = useState(10);
+	const [searchTerm, setSearchTerm] = useState("");
 
-      const handleChangePage = (event, newPage) => {
-            setPage(newPage);
-      };
+	const handleChangePage = (event, newPage) => {
+		setPage(newPage);
+	};
 
-      const handleChangeRowsPerPage = (event) => {
-            setRowsPerPage(+event.target.value);
-            setPage(0);
-      };
+	const handleChangeRowsPerPage = (event) => {
+		setRowsPerPage(+event.target.value);
+		setPage(0);
+	};
 
-      return (
-            <Paper sx={{ width: "100%", overflow: "hidden", marginTop: "3rem" }}>
-                  <TableContainer sx={{ maxHeight: 440 }}>
-                        <Table>
-                              <TableHead>
-                                    <TableRow>
-                                          {columns.map((col) => (
-                                                <TableCell
-                                                      key={col.id}
-                                                      align={col.align}
-                                                      style={{ minWidth: col.minWidth }}
-                                                >
-                                                      {col.label}
-                                                </TableCell>
-                                          ))}
-                                          <TableCell align={"center"}>Actions</TableCell>
-                                    </TableRow>
-                              </TableHead>
-                              <TableBody>
-                                    {rows
-                                          .slice(
-                                                page * rowsPerPage,
-                                                page * rowsPerPage + rowsPerPage
-                                          )
-                                          .map((row) => (
-                                                <TableRow hover key={row._id}>
-                                                      {columns.map((col) => (
-                                                            <TableCell
-                                                                  key={col.id + row._id}
-                                                                  align={col.align}
-                                                            >
-                                                                  {row[col.id]}
-                                                            </TableCell>
-                                                      ))}
-                                                      <TableCell>
-                                                            <TableActions
-                                                                  getView={() =>
-                                                                        getViewHandler(row._id)
-                                                                  }
-                                                                  getEdit={() =>
-                                                                        getEditHandler(row._id)
-                                                                  }
-                                                                  getDelete={() =>
-                                                                        getDeleteHandler(row._id)
-                                                                  }
-                                                                  id={row._id}
-                                                            />
-                                                      </TableCell>
-                                                </TableRow>
-                                          ))}
-                              </TableBody>
-                        </Table>
-                  </TableContainer>
-                  <TablePagination
-                        rowsPerPageOptions={[10, 25, 100]}
-                        component="div"
-                        count={rows.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onPageChange={handleChangePage}
-                        onRowsPerPageChange={handleChangeRowsPerPage}
-                  />
-                  {/* <Pagination
+	const filteredRows = rows.filter((row) => {
+		return Object.values(row).some((value) =>
+			String(value).toLowerCase().includes(searchTerm.toLowerCase())
+		);
+	});
+
+	return (
+		<Paper sx={{ width: "100%", overflow: "hidden", marginTop: "3rem" }}>
+			<TextField
+				label="Search"
+				variant="outlined"
+				margin="normal"
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
+			/>
+			<TableContainer sx={{ maxHeight: 440 }}>
+				<Table>
+					<TableHead>
+						<TableRow>
+							{columns.map((col) => (
+								<TableCell
+									key={col.id}
+									align={col.align}
+									style={{ minWidth: col.minWidth }}
+								>
+									{col.label}
+								</TableCell>
+							))}
+							<TableCell align={"center"}>Actions</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{filteredRows
+							.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+							.map((row) => (
+								<TableRow hover key={row._id}>
+									{columns.map((col) => (
+										<TableCell key={col.id + row._id} align={col.align}>
+											{row[col.id]}
+										</TableCell>
+									))}
+									<TableCell>
+										<TableActions
+											getView={() => getViewHandler(row._id)}
+											getEdit={() => getEditHandler(row._id)}
+											getDelete={() => getDeleteHandler(row._id)}
+											id={row._id}
+										/>
+									</TableCell>
+								</TableRow>
+							))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			<TablePagination
+				rowsPerPageOptions={[10, 25, 100]}
+				component="div"
+				count={rows.length}
+				rowsPerPage={rowsPerPage}
+				page={page}
+				onPageChange={handleChangePage}
+				onRowsPerPageChange={handleChangeRowsPerPage}
+			/>
+			{/* <Pagination
                         count={Math.ceil(rows.length / rowsPerPage)}
                         page={page}
                         onChange={handleChangePage}
@@ -102,8 +104,8 @@ const TableComponent = ({ columns, rows, openForm, formName }) => {
                         onRowsPerPageChange={handleChangeRowsPerPage}
                         size="large"
                   /> */}
-            </Paper>
-      );
+		</Paper>
+	);
 };
 
 export default TableComponent;
