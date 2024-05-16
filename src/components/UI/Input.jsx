7874b0dbf1a7c3b8b17a0322563ba4a2f353fdc0
zeyad sheeha <sheehaph@gmail.com>
2024-05-16 13:20:08 +0300

    just handle some login design

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,40 +1,25 @@
 /* eslint-disable react/prop-types */
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
-const Input = ({
-	half,
-	name,
-	handleChange,
-	label,
-	autoFocus,
-	type,
-	handleShowPassword,
-}) => {
-	return (
-		<div className={`w-full ${half ? "sm:w-1/2" : "sm:w-full"} mb-4`}>
-			<input
-				className="relative w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-400"
-				name={name}
-				onChange={handleChange}
-				placeholder={label}
-				autoFocus={autoFocus}
-				type={type}
-				required
-			/>
-			{name === "password" && (
-				<button
-					onClick={handleShowPassword}
-					className="top-0 right-0 mt-2 mr-3 focus:outline-none"
-				>
-					<FontAwesomeIcon
-						icon={type === "password" ? faEye : faEyeSlash}
-						className="text-gray-500 hover:text-gray-700"
-					/>
-				</button>
-			)}
-		</div>
-	);
+const Input = ({ half, name, handleChange, label, autoFocus, type, handleShowPassword }) => {
+      return (
+            <div className={`w-full ${half ? "sm:w-1/2" : "sm:w-full"} mb-4`}>
+                  <input
+                        className="relative w-full p-4 	border rounded-md focus:outline-none focus:ring focus:ring-blue-400"
+                        name={name}
+                        onChange={handleChange}
+                        placeholder={label}
+                        autoFocus={autoFocus}
+                        type={type}
+                        required
+                  />
+                  {name === "password" && (
+                        <button
+                              onClick={handleShowPassword}
+                              className="top-0 right-0 mt-2 mr-3 focus:outline-none"
+                        ></button>
+                  )}
+            </div>
+      );
 };
 
 export default Input;
