0bafddaabbcab49d6897fe35ba99c3e2af138011
Zeyad Hegazy <zhegazy409@gmail.com>
2024-05-28 00:31:54 +0300

    add button to go back in unauth page

--- src/pages/UnAuthorized.jsx
+++ src/pages/UnAuthorized.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const UnAuthorized = () => {
+	const navigate = useNavigate();
+
 	return (
-		<div>
-			<h1>Unauthorized</h1>
-			<p>You do not have permission to view this page.</p>
+		<div className="h-[100vh] flex flex-col justify-center items-center gap-4">
+			<h1 className="text-4xl">Unauthorized</h1>
+			<p className="text-xl">You do not have permission to view this page.</p>
+			<button
+				onClick={() => navigate(-1)}
+				className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+			>
+				Go Back
+			</button>
 		</div>
 	);
 };
